602db1e5790c4bcdeba66d51f72f94603104284e
Muneeb <umuhammadmuneeb@gmail.com>
2024-12-21 14:09:55 +0500

    added function in string function

--- stringOperations.h
+++ stringOperations.h
@@ -1,1 +1,14 @@
-this update  klkslds
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class StringOperations{
+    public:
+    bool extCheck ( const string &fileName, string const &extension) {
+    // check the file extension ends with .txt
+    if (fileName.length() >= extension.length())
+        return (0 == fileName.compare (fileName.length() - extension.length(), extension.length(), extension));
+    else 
+        return false;
+    }
